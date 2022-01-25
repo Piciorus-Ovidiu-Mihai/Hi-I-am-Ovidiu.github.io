10af0255d816555bad62731eb9dcf7eb005f57b6
Ovidiu-Mihai Piciorus <piciorus.ovidiu.mihai@gmail.com>
2022-01-25 22:53:59 +0200

    parametrize colors

--- src/app/sections/work-experience/work-experience.component.ts
+++ src/app/sections/work-experience/work-experience.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { workExperienceSection } from 'src/constants/app-constants';
 
 @Component({
   selector: 'app-work-experience',
   templateUrl: './work-experience.component.html',
-  styleUrls: ['./work-experience.component.scss']
+  styleUrls: ['./work-experience.component.scss'],
 })
-export class WorkExperienceComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class WorkExperienceComponent {
+  workExperience = workExperienceSection;
 }
--- src/constants/app-constants.ts
+++ src/constants/app-constants.ts
@@ -61,31 +61,57 @@ const aboutMe = {
 };
 
 const facebookLink = 'https://www.facebook.com/ovidiu.mihai.353';
-const linkedinLink = 'https://www.linkedin.com/in/ovidiu-mihai-picioru%C8%99-a0793b19b/';
-const gmailLink = 'https://mail.google.com/mail/?view=cm&fs=1&to=piciorus.ovidiu.mihai@gmail.com';
+const linkedinLink =
+  'https://www.linkedin.com/in/ovidiu-mihai-picioru%C8%99-a0793b19b/';
+const gmailLink =
+  'https://mail.google.com/mail/?view=cm&fs=1&to=piciorus.ovidiu.mihai@gmail.com';
 const githubLink = 'https://github.com/Piciorus-Ovidiu-Mihai';
 
 const skills = {
   title: 'What I do ?',
-  subTitle: "FULL-STACK DEVELOPER WHO LIKES TO BUILD SCALABLE WEB APPLICATION",
+  subTitle: 'FULL-STACK DEVELOPER WHO LIKES TO BUILD SCALABLE WEB APPLICATION',
   skillsList: [
     {
-      info: "Develop scalable web application using Angular and .NET technologies"
+      info: 'Develop scalable web application using Angular and .NET technologies',
     },
     {
-      info: "Build reponsive UI"
+      info: 'Build reponsive UI',
     },
     {
-      info: "Integration of third party services such as Firebase"
+      info: 'Integration of third party services such as Firebase',
     },
     {
-      info: "Building mobile application using Android"
+      info: 'Building mobile application using Android',
     },
     {
-      info: "Highly organized, attention to detail, problem-solving"
-    }
-  ]
-}
+      info: 'Highly organized, attention to detail, problem-solving',
+    },
+  ],
+};
+
+const workExperienceSection = {
+  title: 'Work Experience',
+  jobsInfo: [
+    {
+      title: 'AROBS Software',
+      description: '.NET FULL-STACK student program',
+      timeRange: 'Jul 2021 - Aug 2022',
+      jobTitle: 'Full-stack developer',
+    },
+    {
+      title: 'AROBS Software',
+      description: '.NET FULL-STACK student program',
+      timeRange: 'Jul 2021 - Aug 2022',
+      jobTitle: 'Full-stack developer',
+    },
+    {
+      title: 'AROBS Software',
+      description: '.NET FULL-STACK student program',
+      timeRange: 'Jul 2021 - Aug 2022',
+      jobTitle: 'Full-stack developer',
+    },
+  ],
+};
 
 export {
   developerTitleName,
@@ -96,5 +122,6 @@ export {
   linkedinLink,
   gmailLink,
   githubLink,
-  skills
+  skills,
+  workExperienceSection
 };
